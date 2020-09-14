66a70ca0a6de6e56120cbd6427bc3e03c7fde357
Stephen <stephen_h369@live.co.uk>
2020-09-14 21:02:38 +0100

    FilmReleaseList.js

---
+++ src/components/FilmReleaseList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import FilmRelease from './FilmRelease';
+
+class FilmReleaseList extends Component {
+  render() {
+    const filmNodes = this.props.films.map((film) => {
+      return (
+        <FilmRelease key={film.id} name={film.name} url={film.url}/>
+      );
+    })
+    return (
+      <>
+        <ul className="film-release-list">
+          {filmNodes}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default FilmRelease;
